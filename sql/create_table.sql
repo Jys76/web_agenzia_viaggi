98c3aff1a51f75bd821d420ef1b715630f4a906d
jyu <jiayu.sun@iispadovano.it>
2026-02-24 10:24:15 +0100

    Feat [database]: Create table advancement

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,13 +1,13 @@
 
-CREATE TABLE cont(
+CREATE TABLE IF NOT EXISTS cont(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(64),
     descr VARCHAR(255),
 
     PRIMARY KEY(id)
-)
+);
 
-CREATE TABLE natn(
+CREATE TABLE IF NOT EXISTSnatn(
     id INT NOT NULL AUTO_INCREMENT,
     id_cont INT NOT NULL,
 
@@ -16,9 +16,9 @@ CREATE TABLE natn(
 
     PRIMARY KEY(id),
     FOREIGN KEY(id_cont) REFERENCES cont(id)
-)
+);
 
-CREATE TABLE regn(
+CREATE TABLE IF NOT EXISTS regn(
     id INT NOT NULL AUTO_INCREMENT,
     id_natn INT NOT NULL,
     name VARCHAR(64),
@@ -26,9 +26,9 @@ CREATE TABLE regn(
 
     PRIMARY KEY(id),
     FOREIGN KEY(id_natn) REFERENCES natn(id)
-)
+);
 
-CREATE TABLE prov(
+CREATE TABLE IF NOT EXISTS prov(
     id INT NOT NULL AUTO_INCREMENT,
     id_regn INT NOT NULL,
     name VARCHAR(64),
@@ -36,9 +36,9 @@ CREATE TABLE prov(
 
     PRIMARY KEY(id),
     FOREIGN KEY(id_regn) REFERENCES regn(id)
-)
+);
 
-CREATE TABLE city(
+CREATE TABLE IF NOT EXISTS city(
     id INT NOT NULL AUTO_INCREMENT,
     id_prov INT NOT NULL,
     name VARCHAR(64),
@@ -46,9 +46,9 @@ CREATE TABLE city(
 
     PRIMARY KEY(id),
     FOREIGN KEY(id_prov) REFERENCES prov(id)
-)
+);
 
-CREATE TABLE loct(
+CREATE TABLE IF NOT EXISTS loct(
     id INT NOT NULL AUTO_INCREMENT,
     id_city INT NOT NULL,
     address VARCHAR(128),
@@ -56,29 +56,40 @@ CREATE TABLE loct(
 
     PRIMARY KEY(id),
     FOREIGN KEY(id_city) REFERENCES city(id)
-)
+);
 
 
-
-
-
-
-CREATE TABLE curr(
+CREATE TABLE IF NOT EXISTS curr(
     id VARCHAR(3) NOT NULL,
     descr VARCHAR(255),
 
     PRIMARY KEY(id)
-)
+);
 
-CREATE TABLE sex(
+CREATE TABLE IF NOT EXISTS sex(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(64),
     descr VARCHAR(255),
 
     PRIMARY KEY(id)
-)
+);
 
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS seas(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64) NOT NULL,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id)
+);
+
+
+
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS user(
     id INT NOT NULL AUTO_INCREMENT,
     id_loct INT NOT NULL,
     id_sex INT NOT NULL,
@@ -89,6 +100,82 @@ CREATE TABLE user(
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(14) NOT NULL,
+    cod_fisc VARCHAR(24) NOT NULL,
     
-    CONSTRAINT 
-)
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_loct) REFERENCES loct(id),
+    FOREIGN KEY(id_sex) REFERENCES sex(id),
+    
+    CONSTRAINT uq_user_username UNIQUE (username),
+    CONSTRAINT uq_user_email UNIQUE (email),
+    CONSTRAINT uq_user_phone UNIQUE (phone),
+    CONSTRAINT uq_user_cod_fisc UNIQUE (cod_fisc)
+);
+
+CREATE TABLE IF NOT EXISTS apat(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_loct INT NOT NULL,
+    
+    stars INT NOT NULL,
+    n_roms INT NOT NULL,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_loct) REFERENCES loct(id)
+);
+
+CREATE TABLE IF NOT EXISTS roms_stat(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64) NOT NULL,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS roms(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_apat INT NOT NULL,
+    id_roms_stat INT NOT NULL,
+    
+    beds INT NOT NULL,
+    rom_num INT NOT NULL,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_apat) REFERENCES apat(id),
+    FOREIGN KEY (id_roms_stat) REFERENCES roms_stat(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS trip_catg(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64) NOT NULL,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS trip(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_loct INT NOT NULL,
+    id_curr VARCHAR(3) NOT NULL,
+    id_apat INT NOT NULL,
+    id_trip_catg INT NOT NULL,
+    id_seas INT NOT NULL,
+
+    name VARCHAR(64) NOT NULL,
+    price DECIMAL(10, 2),
+    event_start TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    event_end TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    descr VARCHAR(255),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_loct) REFERENCES loct(id),
+    FOREIGN KEY (id_curr) REFERENCES curr(id),
+    FOREIGN KEY (id_apat) REFERENCES apat(id),
+    FOREIGN KEY (id_trip_catg) REFERENCES trip_catg(id),
+    FOREIGN KEY (id_seas) REFERENCES seas(id)
+);
+
+
+
